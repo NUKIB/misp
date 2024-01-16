0f4b4340936f9afca1788b5252d6130e68e4eb16
ondj <19149300+ondj@users.noreply.github.com>
2024-01-16 01:02:45 +0100

    ecs: Handle escaping in access logs

--- bin/httpd_ecs_log.py
+++ bin/httpd_ecs_log.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # This script converts Apache logs from custom defined format to JSON
 # Generating JSON directly by setting ErrorLogFormat is problematic because of JSON escaping
+import re
 import sys
 import socket
 import argparse
@@ -31,6 +32,7 @@ except ModuleNotFoundError:
 
 
 ECS_VERSION = "8.11"
+DOUBLE_ESCAPE = re.compile(br'(\\x[0-9a-f]{2})')
 
 
 def now():
@@ -102,137 +104,146 @@ def create_generic_error(message: str) -> dict:
     }
 
 
+def convert_access_log_to_ecs(log: dict, logger: EcsLogger) -> dict:
+    # Normalize log by removing dash that indicates empty value from log messages
+    for field in ("log_id", "request_id", "http_x_forwarded_for", "user", "http_referer", "http_location", "user_email"):
+        if field in log and log[field] == "-":
+            log[field] = None
+
+    # Normalize integer values
+    for field in ("pid", "remote_port", "server_port", "bytes_sent", "body_bytes_sent", "status"):
+        if field in log and log[field] == "-":
+            log[field] = None
+        else:
+            try:
+                log[field] = int(log[field])
+            except ValueError:
+                log[field] = None
+                logger.send(create_generic_error(f"Could not convert access log {field} field value {log[field]} to integer"))
+
+    http_version = None
+    if log["server_protocol"][0:5] == "HTTP/":
+        http_version = log["server_protocol"][5:]
+
+    client = {
+        "ip": log["remote_addr"],
+        "port": log["remote_port"],
+    }
+
+    # If HTTP header X-Forwarded-For is present, use it as real IP and hide proxy IP in nat section
+    # X-Forwarded-For can contain multiple values, see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+    if log["http_x_forwarded_for"]:
+        forwarded_for = [value.strip() for value in log["http_x_forwarded_for"].split(",")]
+        forwarded_for.append(log["remote_addr"])
+
+        client = {
+            "address": forwarded_for,
+            "ip": forwarded_for[0],
+            "nat": client,
+        }
+    else:
+        client["address"] = (log["remote_addr"], )
+
+    output = {
+        "@timestamp": log["@timestamp"],
+        "ecs": {
+            "version": ECS_VERSION,
+        },
+        "event": {
+            "category": "web",
+            "type": "access",
+            "kind": "event",
+            "provider": "misp",
+            "module": "httpd",
+            "dataset": "httpd.access",
+            "duration": log["duration"] * 1000,  # in nanoseconds
+        },
+        "process": {
+            "pid": log["pid"],
+        },
+        "client": client,
+        "server": {
+            "domain": log["server_name"],
+            "port": log["server_port"],
+        },
+        "http": {
+            "version": http_version,
+            "request": {
+                "id": log["request_id"],
+                "x_forwarded_for": log["http_x_forwarded_for"],  # custom field
+                "method": log["request_method"],
+                "referer": log["http_referer"],
+            },
+            "response": {
+                "status_code": log["status"],
+                "bytes": log["bytes_sent"],
+                "body": {
+                    "bytes": log["body_bytes_sent"],
+                },
+            },
+        },
+        "url": {
+            "path": log["request_uri"],
+        },
+        "user_agent": {
+            "original": log["http_user_agent"],
+        },
+        "file": {
+            "path": log["file"],
+        }
+    }
+
+    if log["http_location"]:
+        output["http"]["response"]["location"] = log["http_location"]
+
+    if log["host"]:
+        if ":" in log["host"]:
+            domain, port = log["host"].split(":", 1)
+            output["url"]["domain"] = domain
+            output["url"]["port"] = int(port)
+        else:
+            output["url"]["domain"] = log["host"]
+
+    if log["args"]:
+        # According to ECS spec, remove ? from query string
+        output["url"]["query"] = log["args"].lstrip("?")
+
+    if log["user"] or log["user_email"]:
+        output["user"] = {}
+
+        if log["user"]:
+            if "@" in log["user"]:
+                user_id, domain = log["user"].split("@", 1)
+                output["user"]["id"] = user_id
+                output["user"]["domain"] = domain
+            else:
+                output["user"]["id"] = log["user"]
+
+        if log["user_email"]:
+            output["user"]["email"] = log["user_email"]
+
+    if log["log_id"]:
+        output["error"] = {
+            "id": log["log_id"],
+        }
+
+    return output
+
+
 def access_log(logger: EcsLogger):
     for line in sys.stdin.buffer:
         line = line.rstrip(b"\n")
+
+        # Double escape values from httpd, as it is non valid JSON escaping
+        line = DOUBLE_ESCAPE.sub(br'\\\1', line)
+
         try:
             log = json.loads(line)
-        except json.JSONDecodeError:
-            logger.send(create_generic_error(f"Invalid JSON access log received from httpd: {line}"))
+        except json.JSONDecodeError as e:
+            logger.send(create_generic_error(f"Invalid JSON access log received from httpd {e}: {line}"))
             continue
 
-        # Normalize log by removing dash that indicates empty value from log messages
-        for field in ("log_id", "request_id", "http_x_forwarded_for", "user", "http_referer", "http_location", "user_email"):
-            if field in log and log[field] == "-":
-                log[field] = None
-
-        # Normalize integer values
-        for field in ("pid", "remote_port", "server_port", "bytes_sent", "body_bytes_sent", "status"):
-            if field in log and log[field] == "-":
-                log[field] = None
-            else:
-                try:
-                    log[field] = int(log[field])
-                except ValueError:
-                    log[field] = None
-                    logger.send(create_generic_error(f"Could not convert {field} field value {log[field]} to integer"))
-
-        http_version = None
-        if log["server_protocol"][0:5] == "HTTP/":
-            http_version = log["server_protocol"][5:]
-
-        client = {
-            "ip": log["remote_addr"],
-            "port": log["remote_port"],
-        }
-
-        # If HTTP header X-Forwarded-For is present, use it as real IP and hide proxy IP in nat section
-        # X-Forwarded-For can contain multiple values, see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
-        if log["http_x_forwarded_for"]:
-            forwarded_for = [value.strip() for value in log["http_x_forwarded_for"].split(",")]
-            forwarded_for.append(log["remote_addr"])
-
-            client = {
-                "address": forwarded_for,
-                "ip": forwarded_for[0],
-                "nat": client,
-            }
-        else:
-            client["address"] = (log["remote_addr"], )
-
-        output = {
-            "@timestamp": log["@timestamp"],
-            "ecs": {
-                "version": ECS_VERSION,
-            },
-            "event": {
-                "category": "web",
-                "type": "access",
-                "kind": "event",
-                "provider": "misp",
-                "module": "httpd",
-                "dataset": "httpd.access",
-                "duration": log["duration"] * 1000,  # in nanoseconds
-            },
-            "process": {
-                "pid": log["pid"],
-            },
-            "client": client,
-            "server": {
-                "domain": log["server_name"],
-                "port": log["server_port"],
-            },
-            "http": {
-                "version": http_version,
-                "request": {
-                    "id": log["request_id"],
-                    "x_forwarded_for": log["http_x_forwarded_for"],  # custom field
-                    "method": log["request_method"],
-                    "referer": log["http_referer"],
-                },
-                "response": {
-                    "status_code": log["status"],
-                    "bytes": log["bytes_sent"],
-                    "body": {
-                        "bytes": log["body_bytes_sent"],
-                    },
-                },
-            },
-            "url": {
-                "path": log["request_uri"],
-            },
-            "user_agent": {
-                "original": log["http_user_agent"],
-            },
-            "file": {
-                "path": log["file"],
-            }
-        }
-
-        if log["http_location"]:
-            output["http"]["response"]["location"] = log["http_location"]
-
-        if log["host"]:
-            if ":" in log["host"]:
-                domain, port = log["host"].split(":", 1)
-                output["url"]["domain"] = domain
-                output["url"]["port"] = int(port)
-            else:
-                output["url"]["domain"] = log["host"]
-
-        if log["args"]:
-            # According to ECS spec, remove ? from query string
-            output["url"]["query"] = log["args"].lstrip("?")
-
-        if log["user"] or log["user_email"]:
-            output["user"] = {}
-
-            if log["user"]:
-                if "@" in log["user"]:
-                    user_id, domain = log["user"].split("@", 1)
-                    output["user"]["id"] = user_id
-                    output["user"]["domain"] = domain
-                else:
-                    output["user"]["id"] = log["user"]
-
-            if log["user_email"]:
-                output["user"]["email"] = log["user_email"]
-
-        if log["log_id"]:
-            output["error"] = {
-                "id": log["log_id"],
-            }
-
+        output = convert_access_log_to_ecs(log, logger)
         logger.send(output)
 
 
