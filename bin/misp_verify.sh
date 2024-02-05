d6b5c156f5b0e28f0218a4f2c83e26f60ec9f56d
ondj <19149300+ondj@users.noreply.github.com>
2024-02-05 15:12:03 +0100

    php: Verify if extensions are correctly built

--- bin/misp_verify.sh
+++ bin/misp_verify.sh
@@ -6,6 +6,9 @@ set -o xtrace
 # Check if PHP is properly configured
 php -v
 
+# Check if PHP extensions are properly built
+misp_verify.php
+
 # Build test
 # Temporary disable for aarch64, because LIEF is broken in arm
 if [[ "$(uname -m)" != "aarch64" ]]; then
