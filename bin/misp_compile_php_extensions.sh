17b31ddbb99142730c4a2937c9f3450800fd7a12
ondj <19149300+ondj@users.noreply.github.com>
2024-08-30 14:51:40 +0200

    Enable stack protection for PHP extensions

--- bin/misp_compile_php_extensions.sh
+++ bin/misp_compile_php_extensions.sh
@@ -27,6 +27,8 @@ else
   MARCH="";
 fi
 
+DEFAULT_FLAGS="-O2 -g -Wl,-z,relro,-z,now -flto=auto -fstack-protector-strong ${MARCH}"
+
 mkdir /build/php-modules/
 
 # Compile simdjson
@@ -34,7 +36,7 @@ mkdir /tmp/simdjson
 cd /tmp/simdjson
 download_and_check https://github.com/crazyxman/simdjson_php/releases/download/3.0.0/simdjson-3.0.0.tgz 23cdf65ee50d7f1d5c2aa623a885349c3208d10dbfe289a71f26bfe105ea8db9
 phpize
-CPPFLAGS="-flto=auto ${MARCH}" ./configure --silent
+CPPFLAGS="$DEFAULT_FLAGS" ./configure --silent
 make -j$NPROC
 mv modules/*.so /build/php-modules/
 
@@ -43,7 +45,7 @@ mkdir /tmp/igbinary
 cd /tmp/igbinary
 download_and_check https://github.com/igbinary/igbinary/archive/refs/tags/3.2.14.tar.gz 3dd62637667bee9328b3861c7dddc754a08ba95775d7b57573eadc5e39f95ac6
 phpize
-CFLAGS="-O2 -g ${MARCH} -flto=auto" ./configure --silent --enable-igbinary
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent --enable-igbinary
 make -j$NPROC
 make install # `make install` is necessary, so redis extension can be compiled with `--enable-redis-igbinary`
 mv modules/*.so /build/php-modules/
@@ -56,7 +58,7 @@ cd zstd
 download_and_check https://github.com/facebook/zstd/releases/download/v1.5.6/zstd-1.5.6.tar.gz 8c29e06cf42aacc1eafc4077ae2ec6c6fcb96a626157e0593d5e82a34fd403c1
 cd ..
 phpize
-CFLAGS="-g -O2 -flto=auto ${MARCH}" ./configure --silent
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent
 make --silent -j$NPROC
 mv modules/*.so /build/php-modules/
 
@@ -65,7 +67,7 @@ mkdir /tmp/redis
 cd /tmp/redis
 download_and_check https://github.com/phpredis/phpredis/archive/refs/tags/6.0.2.tar.gz 786944f1c7818cc7fd4289a0d0a42ea630a07ebfa6dfa9f70ba17323799fc430
 phpize
-CFLAGS="-flto=auto ${MARCH}" ./configure --silent --enable-redis-igbinary
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent --enable-redis-igbinary
 #./configure --silent --enable-redis-igbinary
 make -j$NPROC
 mv modules/*.so /build/php-modules/
@@ -75,7 +77,7 @@ mkdir /tmp/ssdeep
 cd /tmp/ssdeep
 download_and_check https://github.com/JakubOnderka/pecl-text-ssdeep/archive/3a2e2d9e5d58fe55003aa8b1f31009c7ad7f54e0.tar.gz 275bb3d6ed93b5897c9b37dac358509c3696239f521453d175ac582c81e23cbb
 phpize
-./configure --silent --with-ssdeep=/usr --with-libdir=lib64
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent --with-ssdeep=/usr --with-libdir=lib64
 make -j$NPROC
 mv modules/*.so /build/php-modules/
 
@@ -84,7 +86,7 @@ mkdir /tmp/brotli
 cd /tmp/brotli
 download_and_check https://github.com/kjdev/php-ext-brotli/archive/48bf4071d266c556d61684e07d40d917f61c9eb7.tar.gz c145696965fac0bacd6b5ffef383eaf7a67539e9a0ed8897ab1632ca119510c6
 phpize
-./configure --silent --with-libbrotli
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent --with-libbrotli
 make -j$NPROC
 mv modules/*.so /build/php-modules/
 
@@ -94,7 +96,7 @@ cd /tmp/snuffleupagus
 download_and_check https://github.com/jvoisin/snuffleupagus/archive/refs/tags/v0.10.0.tar.gz 2b48dc38e208e793d3e694688d9ae9fc429e99a8df01133f6e0a4444036ba304
 cd src
 phpize
-CFLAGS="-g -O2 -flto=auto ${MARCH}" ./configure --silent --enable-snuffleupagus
+CFLAGS="$DEFAULT_FLAGS" ./configure --silent --enable-snuffleupagus
 make -j$NPROC
 mv modules/*.so /build/php-modules/
 
--- bin/misp_compile_zlib_ng.sh
+++ bin/misp_compile_zlib_ng.sh
@@ -24,7 +24,7 @@ fi
 mkdir /tmp/zlib-ng
 cd /tmp/zlib-ng
 download_and_check https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.1.tar.gz ec6a76169d4214e2e8b737e0850ba4acb806c69eeace6240ed4481b9f5c57cdf
-CFLAGS="-flto=auto -fstack-protector-strong -D_FORTIFY_SOURCE=2 ${MARCH}" ./configure --zlib-compat
+CFLAGS="-flto=auto -fstack-protector-strong -D_FORTIFY_SOURCE=2 -Wl,-z,relro,-z,now ${MARCH}" ./configure --zlib-compat
 make -j$(nproc)
 strip libz.so.1.3.1.zlib-ng
 mv libz.so.1.3.1.zlib-ng /build/
