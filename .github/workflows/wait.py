7e5a66d7ca935a406a4e5d3f354ad5ff9ea5ed6c
ondj <19149300+ondj@users.noreply.github.com>
2025-05-21 09:02:10 +0200

    ci: Wait until minio is ready

--- .github/workflows/wait.py
+++ .github/workflows/wait.py
@@ -3,20 +3,23 @@ import sys
 import time
 import argparse
 import urllib.request
-from http.client import HTTPResponse
+import urllib.error
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url")
+parser.add_argument("--ignore-http-error", action="store_true")
 args = parser.parse_args()
 
 max_tries = 30
 while True:
     try:
-        resp: HTTPResponse = urllib.request.urlopen(args.url, timeout=1)
-        if 200 <= resp.getcode() < 300:
-            break
-        else:
-            raise Exception("Invalid response code {}".format(resp.getcode()))
+        try:
+            urllib.request.urlopen(args.url, timeout=1)
+        except urllib.error.HTTPError:
+            if args.ignore_http_error:
+                break
+            raise
+        break
     except Exception as e:
         max_tries -= 1
         if max_tries == 0:
